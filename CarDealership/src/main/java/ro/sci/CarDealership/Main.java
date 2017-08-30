c897bc0ec69ca23febaf57fbecd65b3615ff0727
Desktop-home <cafeaua@gmail.com>
2017-08-30 15:47:55 +0300

    A minor modification to the files, so I can find them on GitHub

--- CarDealership/src/main/java/ro/sci/CarDealership/Main.java
+++ CarDealership/src/main/java/ro/sci/CarDealership/Main.java
@@ -6,7 +6,7 @@ package ro.sci.CarDealership;
 public class Main {
 
     public static void main(String[] args) {
-        Car myNewCar = new Car(2017, "Honda", "Civic",
+        Car myNewCar = new Car(2016, "Honda", "Civic",
                 "8BHJSD0", 56.3, 44.4, 82.0, true);
         System.out.println(myNewCar.carToSring());
     }
