36ff42e454a7e4a9ec1913cae41299e5fcec8575
Sipos Zoltan <cafeaua@gmail.com>
2017-09-27 21:08:34 +0300

    Homework4

---
+++ Java G8/ElectricCarSales/src/main/java/ro/sci/Dealership/Dealership.java
@@ -0,0 +1,32 @@
+package ro.sci.Dealership;
+
+public class Dealership {
+
+    int[] carFleet;
+    int stockNumber;
+    float carPrice;
+}
+//5 Java OOP Homework
+//
+//        EV Purchase Program Milestone 2 - Java OOP Implementation
+//        Implement relationships between classes from milestone 1.
+//
+//        You do not need to implement any behavior (i.e selling / buying a car),
+//        however the Aggregation and Containment relationships along with any Inheritance must be visible in the code.
+//
+//  Milestone 1 - OOP Design
+
+//        Create simple UML like diagram of the classes in the system.
+//        The diagram should showcase the OOP relationship between the classes.
+//        The OOP Design must cover the following:
+
+//        Dealership OOP Model:
+
+//        represents at least one Manufacturer.
+//        has a fleet of cars, new and used.
+//        has stock nr and price for each car
+
+//        Green Bonus Program
+
+//        Has a fixed budget
+//        Has a history of all purchases via the program (client name, dealership name, car model)
