c50eef42b1112c420662349fd9c3f2ec33ac4855
Sipos Zoltan <cafeaua@gmail.com>
2017-10-12 17:23:53 +0300

    Added  ev2 (and working from a different computer directly in the github folder.)

--- Java G8/ElectricCarSales/src/main/java/ro/sci/Dealership/Main.java
+++ Java G8/ElectricCarSales/src/main/java/ro/sci/Dealership/Main.java
@@ -6,6 +6,8 @@ public class Main {
     public static void main(String[] args) {
         Car ev1 = new Car("Dacia", "Logan-e", "dlgne-01", 100.5, 30.4,
                 40, 2017, false);
+        Car ev2 = new Car("BMW", "i3", "bmwi3-01", 200, 50,
+                75.5, 2017, false);
         System.out.println(ev1.secondHand);
     }
 }
